aca181f749c4426550194955787017d87577497b
Amalagu <simonamalagu4@gmail.com>
2023-09-06 18:17:31 +0100

    message

--- portalproject/portal/admin.py
+++ portalproject/portal/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Year, Course, Profile
 # Register your models here.
+admin.site.register(Year)
+admin.site.register(Course)
+admin.site.register(Profile)
--- portalproject/portal/models.py
+++ portalproject/portal/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Year(models.Model):
+	levels= [('Year One', 'YR1'), ('Year Two', 'YR2')]
+	name = models.Integerfield(max_length=10, choices=levels)
+	
+	
+class Course(models.Model):
+	gradelist=[('A', 5), ('B', 4), ('C', 3)]
+	code=models.CharField(max_length=6, blank=False, null=False)
+	title = models.CharField(max_length=100, blank=False, null=False)
+	level = models.ForeignKey(Year, null=False, blank=False)
+	unit = models.IntegerField()
+	grade=models.IntegerField(choices=gradelist)
+	
+	
+class Profile(models.Model):
+	name = models.CharField(max_length=50, null=False, blank=False)
+	
